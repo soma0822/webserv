44f65a7ff7b4e9a81a7e6991645b0e162324cb87
tkuramot <mabdman512@gmail.com>
2024-02-16 21:36:26 +0900

    includeとsrcディレクトリをまとめる

--- src/example.cpp
+++
@@ -1,9 +0,0 @@
-#include "example.hpp"
-
-int add(int a, int b) {
-  return a + b;
-}
-
-int subtract(int a, int b) {
-  return a - b;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include "example.hpp"
-
 int main() {
-  std::cout << add(1, 3) << std::endl;
-  std::cout << subtract(1, 3) << std::endl;
   return 0;
 }
