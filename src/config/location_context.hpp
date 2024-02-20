5bd4cac4516fbee84b32678711f2c7f687c955da
sinagaki <soma0822soma@gmail.com>
2024-02-20 23:10:18 +0900

    make location context

--- src/config/location_context.hpp
+++ src/config/location_context.hpp
@@ -2,8 +2,47 @@
 #define LOCATION_CONTEXT_HPP
 
 #include <iostream>
+#include <vector>
+#include <map>
 
 class LocationContext{
+public:
+  LocationContext();
+  LocationContext(const LocationContext& other);
+  LocationContext &operator=(const LocationContext& other);
+  //ゲッター
+  bool get_can_auto_index() const;
+  int get_limit_client_body() const;
+  const std::string& get_return() const;
+  const std::string& get_alias() const;
+  const std::string& get_root() const;
+  const std::vector<std::string>& get_index() const;
+  const std::vector<std::string>& get_cgi_path() const;
+  const std::vector<std::string>& get_cgi_extention() const;
+  const std::map<std::string, bool>& get_allow_method() const;
+  const std::map<std::string, std::string>& get_error_page() const;
+  //セッター
+  void set_can_auto_index(bool);
+  void set_limit_client_body(int);
+  void set_return(const std::string &);
+  void set_alias(const std::string &);
+  void set_root(const std::string &);
+  void add_index(const std::string &);
+  void add_cgi_path(const std::string &);
+  void add_cgi_extention(const std::string &);
+  void add_allow_method(const std::string &);
+  void add_errorPage(const std::string &, const std::string &);
 
+private:
+  bool can_auto_index_;
+  int limit_client_body_;
+  std::string return_;
+  std::string alias_;
+  std::string root_;
+  std::vector<std::string> index_;
+  std::vector<std::string> cgi_path_;
+  std::vector<std::string> cgi_extention_;
+  std::map<std::string, bool> allow_method_;
+  std::map<std::string, std::string> error_page_;
 };
 #endif
