1ea41f588900f2b5a0cc6a62a9fce6159d402b57
sinagaki <soma0822soma@gmail.com>
2024-02-20 20:28:53 +0900

    make config parse

--- src/config/config_parser.cpp
+++ src/config/config_parser.cpp
@@ -2,5 +2,22 @@
 
 std::vector<ServerContext> ConfigParser::parse(std::ifstream &inf){
   std::vector<ServerContext> server;
+  std::string line;
 
+	while (std::getline(inf, line)){
+    std::stringstream ss(line);
+    std::string key;
+    std::string value;
+		ss >> key;
+    if (key.empty())
+      continue ;
+    ss >> value;
+    if (key + value == "server{" && ss.get() == std::char_traits<char>::eof())
+      ServerParser::parse_server(line);
+    else{
+      std::cerr << "Syntax Error: not server" << std::endl;
+      throw std::exception();
+    }
+
+	}
 }
--- src/config/config_parser.hpp
+++ src/config/config_parser.hpp
@@ -3,6 +3,8 @@
 
 #include <fstream>
 #include <iostream>
+# include <sstream>
+#include <string>
 #include <vector>
 #include "server_context.hpp"
 
