d93567892827e5705a778628a700f53d84a3295f
tkuramot <mabdman512@gmail.com>
2024-02-23 18:09:52 +0900

    YYYY-MM-DDThh:mm:ss形式の文字列を作る

---
+++ src/utils/datetime.cpp
@@ -0,0 +1,12 @@
+#include "datetime.hpp"
+
+namespace datetime {
+std::string GetDatetimeString() {
+  time_t t = std::time(NULL);
+  struct tm *local = std::localtime(&t);
+
+  char buf[20];
+  std::strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", local);
+  return std::string(buf);
+}
+} // namespace datetime
---
+++ src/utils/datetime.hpp
@@ -0,0 +1,5 @@
+#include <string>
+
+namespace datetime {
+std::string GetDatetimeString();
+} // namespace datetime
