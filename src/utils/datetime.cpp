af0a147045453fdf53f549d733dceca640a6d8b1
tkuramot <mabdman512@gmail.com>
2024-02-23 18:14:48 +0900

    namespaceの修正

--- src/utils/datetime.cpp
+++ src/utils/datetime.cpp
@@ -3,10 +3,10 @@
 namespace datetime {
 std::string GetDatetimeString() {
   time_t t = time(NULL);
-  struct tm *local = std::localtime(&t);
+  struct tm *local = localtime(&t);
 
   char buf[20];
-  std::strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", local);
+  strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", local);
   return std::string(buf);
 }
 } // namespace datetime
