e94c7c3972ceeabf3306493fd25ed5d71b6767bb
sinagaki <soma0822soma@gmail.com>
2024-02-27 15:08:28 +0900

    add str_to_i

---
+++ src/utils/str_to_i.hpp
@@ -0,0 +1,10 @@
+#ifndef WEBSERV_UTILS_STR_TO_I_HPP
+#define WEBSERV_UTILS_STR_TO_I_HPP
+
+#include <string>
+#include "result.hpp"
+#include "validation.hpp"
+
+Result<int, std::string> StrToI(const std::string &str);
+
+#endif
