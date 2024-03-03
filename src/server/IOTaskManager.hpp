c00ef9f23c781c4a83ceb92a4fd3ca46cf441799
sinagaki <soma0822soma@gmail.com>
2024-03-03 23:54:37 +0900

    フォーマット走らせた

--- src/server/IOTaskManager.hpp
+++ src/server/IOTaskManager.hpp
@@ -1,12 +1,13 @@
 #ifndef WEBSERV_SRC_SERVER_IOTASKMANAGER_HPP
 #define WEBSERV_SRC_SERVER_IOTASKMANAGER_HPP
 
-#include <iostream>
-#include <vector>
 #include <poll.h>
 
-#include "io_task.hpp"
+#include <iostream>
+#include <vector>
+
 #include "accept.hpp"
+#include "io_task.hpp"
 #include "read_request_from_client.hpp"
 #include "write_response_to_client.hpp"
 
@@ -22,12 +23,9 @@ class IOTaskManager {
   IOTaskManager(const IOTaskManager &);
   IOTaskManager &operator=(const IOTaskManager &);
   static std::vector<std::vector<AIOTask *> > tasks_;
-  static std::vector <struct pollfd> fds_;
+  static std::vector<struct pollfd> fds_;
 
-  enum Error {
-    Ok,
-    Delete
-  };
+  enum Error { Ok, Delete };
 };
 
 #endif
