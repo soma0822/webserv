feacfa2fe404ded33c715c42148d3312c1a12097
soma0822 <120237021+soma0822@users.noreply.github.com>
2024-03-03 11:20:12 +0900

    mapやめた (#84)
    * mapやめた
    
    * 見つからないときはから文字列返す
    
    * switchのスコープミスった　”

--- src/http/http_response.cpp
+++ src/http/http_response.cpp
@@ -38,7 +38,6 @@ const std::map<std::string, std::string> &HTTPResponse::GetHeaders() const {
 void HTTPResponse::SetBody(const std::string &body) { body_ = body; }
 
 std::string HTTPResponse::ToString() const {
-  std::map<http::StatusCode, std::string>::const_iterator it =
-      http::kStatusMessage.find(status_code_);
-  return "HTTP/1.1 " + it->second + " OK\n\nHello, World!";
+  return "HTTP/1.1 " + http::GetStatusMessage(status_code_) +
+         " OK\n\nHello, World!";
 }
