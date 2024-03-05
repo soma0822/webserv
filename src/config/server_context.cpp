8537547ef326e53c4a020b48fd3b6ac32ea5b75d
sinagaki <soma0822soma@gmail.com>
2024-03-05 16:19:23 +0900

    フォーマット

--- src/config/server_context.cpp
+++ src/config/server_context.cpp
@@ -18,7 +18,6 @@ ServerContext &ServerContext::operator=(const ServerContext &other) {
   return *this;
 }
 
-
 bool ServerContext::HavePort(const std::string &port) {
   if (port_.end() == std::find(port_.begin(), port_.end(), port)) return false;
   return true;
