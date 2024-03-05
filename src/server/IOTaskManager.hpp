5f4330705b598d8ef5e821f7d245659f5045166f
soma0822 <120237021+soma0822@users.noreply.github.com>
2024-03-05 14:28:27 +0900

    47 iotaskmanagerクラスを作る (#92)
    * IOTaskManegerを作った
    
    * フォーマット走らせた

---
+++ src/server/IOTaskManager.hpp
@@ -0,0 +1,31 @@
+#ifndef WEBSERV_SRC_SERVER_IOTASKMANAGER_HPP
+#define WEBSERV_SRC_SERVER_IOTASKMANAGER_HPP
+
+#include <poll.h>
+
+#include <iostream>
+#include <vector>
+
+#include "accept.hpp"
+#include "io_task.hpp"
+#include "read_request_from_client.hpp"
+#include "write_response_to_client.hpp"
+
+class IOTaskManager {
+ public:
+  ~IOTaskManager();
+  static void AddTask(AIOTask *task);
+  static void RemoveTask(AIOTask *task);
+  static void ExecuteTasks();
+
+ private:
+  IOTaskManager();
+  IOTaskManager(const IOTaskManager &);
+  IOTaskManager &operator=(const IOTaskManager &);
+  static std::vector<std::vector<AIOTask *> > tasks_;
+  static std::vector<struct pollfd> fds_;
+
+  enum Error { Ok, Delete };
+};
+
+#endif
