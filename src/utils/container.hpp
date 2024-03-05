150b410a98a3e7ba8dc95b57b6b87075c999f9b9
soma0822 <120237021+soma0822@users.noreply.github.com>
2024-03-05 18:18:32 +0900

    constをやめた (#105)

--- src/utils/container.hpp
+++ src/utils/container.hpp
@@ -6,10 +6,10 @@
 // コンテナと区切り文字を受け取り、コンテナの要素を区切り文字で結合した文字列を返す
 namespace container {
 template <typename T>
-std::string MergeContainer(const T &container, const std::string &sed) {
+std::string MergeContainer(T &container, const std::string &sed) {
   std::stringstream ss;
   std::string result;
-  for (typename T::const_iterator it = container.begin(); it != container.end();
+  for (typename T::iterator it = container.begin(); it != container.end();
        ++it) {
     ss << *it;
     if (it != --container.end()) {
