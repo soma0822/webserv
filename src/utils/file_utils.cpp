e0b0ecbc52397b07c1a71635536a00117ef2542d
tkuramot <mabdman512@gmail.com>
2024-03-05 18:12:04 +0900

    テストを追加

--- src/utils/file_utils.cpp
+++ src/utils/file_utils.cpp
@@ -5,6 +5,7 @@
 
 Result<std::string, file_utils::Error> file_utils::ReadFile(
     const std::string &file_path) {
+  system("pwd");
   std::ifstream ifs(file_path.c_str());
   if (!ifs.is_open()) {
     return Err(kFileOpenError);
