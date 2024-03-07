e8156c8f3ac2208011cf9a7d2056a44f60675a2f
sinagaki <soma0822soma@gmail.com>
2024-03-07 17:17:54 +0900

    have直した

--- src/config/server_context.cpp
+++ src/config/server_context.cpp
@@ -19,15 +19,11 @@ ServerContext &ServerContext::operator=(const ServerContext &other) {
 }
 
 bool ServerContext::HavePort(const std::string &port) const {
-  if (port_.end() == std::find(port_.begin(), port_.end(), port)) return false;
-  return true;
+  return (port_ == port);
 }
 
 bool ServerContext::HaveServerName(const std::string &server_name) const {
-  if (server_name_.end() ==
-      std::find(server_name_.begin(), server_name_.end(), server_name))
-    return false;
-  return true;
+  return (server_name_ == server_name);
 }
 
 // ポートを指定しないサーバー設定はエラー。他はなくてもいい。portについてもデフォルト決めるのもあり
