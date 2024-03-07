77ca2a2e0de8f9de8f335ddb5bb439b02456cd9b
kaaaaakun_ <you@example.com>
2024-03-07 17:41:54 +0900

    fix: enumを変更したためtestも変更

--- test/request_parser_test.cpp
+++ test/request_parser_test.cpp
@@ -25,10 +25,10 @@ TEST(HTTPRequestParser, ParseRequestGET_Header_kNotEnough) {
   HTTPRequestParser parser;
   std::string request = "GET / HTTP/1.1\r\n";
   Result<HTTPRequest *, int> req = parser.Parser(request);
-  EXPECT_EQ(req.UnwrapErr(), kNotEnough);
+  EXPECT_EQ(req.UnwrapErr(), HTTPRequestParser::kNotEnough);
   request = "Host: localhost:8080\r\n";
   Result<HTTPRequest *, int> req2 = parser.Parser(request);
-  EXPECT_EQ(req2.UnwrapErr(), kNotEnough);
+  EXPECT_EQ(req2.UnwrapErr(), HTTPRequestParser::kNotEnough);
   request = "\r\n";
   Result<HTTPRequest *, int> req1 = parser.Parser(request);
   EXPECT_EQ(req1.Unwrap()->GetMethod(), "GET");
@@ -46,27 +46,27 @@ TEST(HTTPRequestParser, ParseRequestGET_Requestline_BadRequest) {
   // methodがない
   std::string request = " / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req = parser.Parser(request);
-  EXPECT_EQ(req.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // pathがない
   request = "GET HTTP/1.1\r\nHost: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req3 = parser.Parser(request);
-  EXPECT_EQ(req3.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req3.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // protocolがない
   request = "GET / /1.1\r\nHost: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req7 = parser.Parser(request);
-  EXPECT_EQ(req7.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req7.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // protocolとversionの間の/がない
   request = "GET / HTTP1.1\r\nHost: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req4 = parser.Parser(request);
-  EXPECT_EQ(req4.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req4.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // versionがない
   request = "GET / HTTP/\r\nHost: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req5 = parser.Parser(request);
-  EXPECT_EQ(req5.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req5.UnwrapErr(), HTTPRequestParser::kBadRequest);
   //\r\nがない
   request = "GET / HTTP/1.1Host: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req6 = parser.Parser(request);
-  EXPECT_EQ(req6.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req6.UnwrapErr(), HTTPRequestParser::kBadRequest);
 }
 
 // headerエラーケース
@@ -76,21 +76,21 @@ TEST(HTTPRequestParser, ParseRequestGET_Header_BadRequest) {
   // Hostヘッダがない
   request = "GET / HTTP/1.1\r\n\r\n";
   Result<HTTPRequest *, int> req7 = parser.Parser(request);
-  EXPECT_EQ(req7.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req7.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // Hostヘッダがない
   request = "GET / HTTP/1.1\r\n: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req8 = parser.Parser(request);
-  EXPECT_EQ(req8.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req8.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // Hostの中身がない
   request = "GET / HTTP/1.1\r\nHost:\r\n\r\n";
   Result<HTTPRequest *, int> req9 = parser.Parser(request);
-  EXPECT_EQ(req9.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req9.UnwrapErr(), HTTPRequestParser::kBadRequest);
   // Hostがない
   request =
       "GET / HTTP/1.1\r\nIf-Modified-Since: Thu, "
       "14 Jun 2018 10:00:00 GMT\r\n\r\n";
   Result<HTTPRequest *, int> req6 = parser.Parser(request);
-  EXPECT_EQ(req6.UnwrapErr(), kBadRequest);
+  EXPECT_EQ(req6.UnwrapErr(), HTTPRequestParser::kBadRequest);
 }
 
 // POSTリクエストのパース
@@ -99,10 +99,10 @@ TEST(HTTPRequestParser, ParseRequestPOST) {
   std::string request =
       "POST / HTTP/1.1\r\nHost: localhost:8080\r\nContent-Length: 5\r\n\r\n";
   Result<HTTPRequest *, int> req = parser.Parser(request);
-  EXPECT_EQ(req.UnwrapErr(), kNotEnough);
+  EXPECT_EQ(req.UnwrapErr(), HTTPRequestParser::kNotEnough);
   request = "he";
   Result<HTTPRequest *, int> req1 = parser.Parser(request);
-  EXPECT_EQ(req1.UnwrapErr(), kNotEnough);
+  EXPECT_EQ(req1.UnwrapErr(), HTTPRequestParser::kNotEnough);
   request = "llo\r\n";
   Result<HTTPRequest *, int> req2 = parser.Parser(request);
   EXPECT_EQ(req2.Unwrap()->GetMethod(), "POST");
