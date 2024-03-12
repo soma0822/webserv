da3cdd0a83e90c02db33203b8364dc107743ea39
sinagaki <soma0822soma@gmail.com>
2024-03-12 14:32:36 +0900

    Acceptでのfcntl

--- src/server/accept.cpp
+++ src/server/accept.cpp
@@ -11,13 +11,18 @@ Accept &Accept::operator=(const Accept &other) {
   return *this;
 }
 
+//ここでのエラーは流すのでkOkで返すことでServer側で何もしないようにしている
 Result<int, std::string> Accept::Execute() {
   struct sockaddr_in client_addr;
   socklen_t len = sizeof(client_addr);
   int client_sock = accept(fd_, (struct sockaddr *)&client_addr, &len);
   if (client_sock == -1) {
     Logger::Error() << "accept エラー" << std::endl;
-    return Err("accept error");
+    return Ok(kOk);
+  }
+  if (fcntl(client_sock, F_SETFL, O_NONBLOCK, FD_CLOEXEC) == -1){
+    close(client_sock);
+    return Ok(kOk);
   }
   Logger::Info() << port_ << " : 接続しました" << std::endl;
   IOTaskManager::AddTask(
