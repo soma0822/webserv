a0dfe53e9dd1c8f281d48a64ce90d940f38499a2
tkuramot <106866329+tkuramot@users.noreply.github.com>
2024-03-18 15:36:51 +0900

    POST (#172)
    * POSTの基本的な実装
    
    * リクエストターゲットがディレクトリだった場合の処理
    
    * リクエストターゲットの親ディレクトリが存在しないときの処理
    
    * ディレクトリに書き込み権限がないときの処理
    
    * ファイルのアップロード
    
    * テストを追加 close #39
    
    * フォーマット
    
    * configが後々必要になるので渡す
    
    * テストOK
    
    * indexファイルの優先順位を変更

--- src/http/request_handler.hpp
+++ src/http/request_handler.hpp
@@ -9,17 +9,16 @@ class RequestHandler {
  public:
   static HTTPResponse *Handle(const IConfig &config, const HTTPRequest *request,
                               const std::string &port, const std::string &ip);
-  static HTTPResponse *Get(const IServerContext &server_ctx,
-                           const HTTPRequest *request);
+  static HTTPResponse *Get(const IConfig &config, const HTTPRequest *request,
+                           const std::string &port, const std::string &ip);
+  static HTTPResponse *Post(const IConfig &config, const HTTPRequest *request,
+                            const std::string &port, const std::string &ip);
 
  private:
   RequestHandler();
   RequestHandler(const RequestHandler &other);
   RequestHandler &operator=(const RequestHandler &other);
   ~RequestHandler();
-
-  static std::string ResolvePath(const IServerContext &server_ctx,
-                                 const std::string &uri);
 };
 
 #endif  // WEBSERV_SRC_HTTP_REQUEST_HANDLER_HPP_
