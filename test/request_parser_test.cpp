a01fb71ec5f4a45f7ff8a494cf5cfa5894753566
kaaaaakun_ <you@example.com>
2024-03-22 16:52:14 +0900

    test :add test

--- test/request_parser_test.cpp
+++ test/request_parser_test.cpp
@@ -94,6 +94,14 @@ TEST(HTTPRequestParser, ParseRequestGET_Requestline_BadRequest) {
   request = "GET / HTTP/1.1Host: localhost:8080\r\n\r\n";
   Result<HTTPRequest *, int> req6 = parser.Parser(request);
   EXPECT_EQ(req6.UnwrapErr(), HTTPRequestParser::kHttpVersionNotSupported);
+  // versionが違う
+  request = "GET / HTTP/1.0\r\nHost: localhost:8080\r\n\r\n";
+  Result<HTTPRequest *, int> req8 = parser.Parser(request);
+  EXPECT_EQ(req8.UnwrapErr(), HTTPRequestParser::kHttpVersionNotSupported);
+  // protocolが違う
+  request = "GET / HTTPS/1.0\r\nHost: localhost:8080\r\n\r\n";
+  Result<HTTPRequest *, int> req9 = parser.Parser(request);
+  EXPECT_EQ(req9.UnwrapErr(), HTTPRequestParser::kBadRequest);
 }
 
 // headerエラーケース
