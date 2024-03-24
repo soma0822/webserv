38979a06d52f0f1a1ff091891f1cf56b9f6c14f9
tkuramot <106866329+tkuramot@users.noreply.github.com>
2024-03-24 13:57:09 +0900

    optionをnewなしで使えるようにする close #239 (#240)
    * optionをnewなしで使えるようにする close #239
    
    * copy constructor, copy assignment operatorの実装

--- src/utils/option.hpp
+++ src/utils/option.hpp
@@ -6,49 +6,41 @@
 template <typename T>
 class Option {
  public:
-  Option() {}
-  Option(const Option &obj) { (void)obj; }
+  Option(T value, bool has_value) : value_(value), has_value_(has_value) {}
+
+  Option(const Option &other)
+      : value_(other.value_), has_value_(other.has_value_) {}
+
   virtual ~Option() {}
-  Option &operator=(const Option &obj) {
-    (void)obj;
+  Option &operator=(const Option &other) {
+    other.value_ = value_;
+    other.has_value_ = has_value_;
     return *this;
   }
 
-  virtual T Unwrap() = 0;
-  virtual bool IsSome() = 0;
-};
-
-template <typename T>
-class None : public Option<T> {
- public:
-  None() : Option<T>() {}
-  None(const Option<T> &obj) : Option<T>(obj) {}
-  ~None() {}
-  None &operator=(const None<T> &obj) {
-    Option<T>::operator=(obj);
-    return *this;
+  T Unwrap() {
+    if (has_value_) {
+      return value_;
+    } else {
+      throw std::runtime_error("Option does not contain a value");
+    }
   }
 
-  T Unwrap() { throw std::runtime_error("Invalid value"); }
-  bool IsSome() { return false; }
-};
-
-template <typename T>
-class Some : public Option<T> {
- public:
-  Some() {}
-  Some(T value) : Option<T>(), value_(value) {}
-  Some(const Some<T> &obj) : Option<T>(obj), value_(obj.value_) {}
-  Some &operator=(const Some<T> &obj) {
-    value_ = obj.value_;
-    return *this;
-  }
-
-  T Unwrap() { return value_; }
-  bool IsSome() { return true; }
+  bool IsSome() { return has_value_; }
 
  private:
   T value_;
+  bool has_value_;
 };
+
+template <typename T>
+Option<T> None() {
+  return Option<T>(T(), false);
+}
+
+template <typename T>
+Option<T> Some(T value) {
+  return Option<T>(value, true);
+}
 
 #endif  // WEVSERB_SRC_UTILS_OPTION_HPP
