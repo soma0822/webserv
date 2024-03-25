1742245226fc09c98235df953b67997874c50c8f
soma0822 <120237021+soma0822@users.noreply.github.com>
2024-03-25 15:34:20 +0900

    256 全体が動くようにする (#265)
    * 動くようになった
    
    * format
    
    * merge
    
    * 毎回先頭から送ってしまっていた。
    
    * ファイル構成変えた

--- src/server/write_response_to_client.cpp
+++ src/server/write_response_to_client.cpp
@@ -14,14 +14,19 @@ WriteResponseToClient::~WriteResponseToClient() {
 
 Result<int, std::string> WriteResponseToClient::Execute() {
   std::string response_str = response_->ToString();
-  int bytes_written = write(fd_, response_str.c_str(), response_str.size());
+  int bytes_written = write(fd_, response_str.c_str() + wrote_size_,
+                            response_str.size() - wrote_size_);
   if (bytes_written == -1) {
     Logger::Error() << "write エラー" << std::endl;
     return Ok(kFdDelete);
   }
   if ((wrote_size_ += bytes_written) == response_str.size()) {
+    Logger::Info() << "レスポンスを書き込みました: " << response_str
+                   << std::endl;
     if (response_->GetStatusCode() == http::kBadRequest) return Ok(kFdDelete);
     return Ok(kTaskDelete);
   }
+  Logger::Info() << bytes_written << " / " << response_str.size()
+                 << "書き込みました" << std::endl;
   return Ok(kContinue);
 }
