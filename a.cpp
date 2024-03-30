79eb6055c61080cd829e71e00054d7f145fdc15f
sinagaki <soma0822soma@gmail.com>
2024-03-30 15:01:06 +0900

    delete a.cpp

--- a.cpp
+++
@@ -1,9 +0,0 @@
-#include "ofstream"
-#include "iostream"
-
-int main() {
-  std::ofstream ofs("a.txt");
-  if (!ofs) {
-    return Some(GenerateErrorResponse(http::kInternalServerError, config));
-  }
-}
