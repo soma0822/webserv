e12b568df6d1d95482f3a2c35a59d0d10380a462
soma0822 <120237021+soma0822@users.noreply.github.com>
2024-03-31 14:21:48 +0900

    317 writereadのエラーハンドリング (#346)
    * writeのエラーハンドリングの追加
    
    * format

--- src/server/write_response_to_client.cpp
+++ src/server/write_response_to_client.cpp
@@ -17,7 +17,7 @@ Result<int, std::string> WriteResponseToClient::Execute(int revent) {
   std::string response_str = response_->ToString();
   int bytes_written = write(fd_, response_str.c_str() + wrote_size_,
                             response_str.size() - wrote_size_);
-  if (bytes_written == -1) {
+  if (bytes_written == -1 || bytes_written == 0) {
     Logger::Error() << "write エラー" << std::endl;
     return Ok(kFdDelete);
   }
