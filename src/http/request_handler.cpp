34123d3c0f45db53f44719485c6622e526a4be0f
tkuramot <mabdman512@gmail.com>
2024-04-10 18:59:35 +0900

    AutoIndexの修正 fix #386

--- src/http/request_handler.cpp
+++ src/http/request_handler.cpp
@@ -77,8 +77,9 @@ Option<HTTPResponse *> RequestHandler::Get(const IConfig &config,
   }
 
   if (need_autoindex) {
-    // ファイルが存在しない場合には404を返す
-    if (!file_utils::CheckIfFileExistsWithoutExecPermission(
+    // ディレクトリが存在しない場合には404を返す
+    Logger::Debug() << "===============" << request_file_path << std::endl;
+    if (!file_utils::DoesFileOrDirectoryExist(
             request_file_path)) {
       return Some(GenerateErrorResponse(http::kNotFound, config));
     }
